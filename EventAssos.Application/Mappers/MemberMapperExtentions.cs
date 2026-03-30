5c133bc2915dc62aa38a71a31f09b2e5c11a1755
paolo <pao.sarno@gmail.com>
2026-03-30 15:32:02 +0200

    update MemberMapperExtentions

--- EventAssos.Application/Mappers/MemberMapperExtentions.cs
+++ EventAssos.Application/Mappers/MemberMapperExtentions.cs
@@ -1,10 +1,26 @@
-﻿using EventAssos.Application.DTOs.Responses;
+﻿using EventAssos.Application.DTOs.Requests;
+using EventAssos.Application.DTOs.Responses;
 using EventAssos.Domain.Entities;
+using EventAssos.Domain.ValueObjects;
+using EventAssos.Domain.Enums;
 
 namespace EventAssos.Application.Mappers
 {
     public static class MemberMapperExtentions
     {
+        public static Member ToMember(this AddMemberRequestDTO request)
+        {
+            return new Member
+            {
+                Id = Guid.NewGuid(),
+                Pseudo = request.Pseudo,
+                EmailAddress = EmailAddress.Create(request.EmailAddress),
+                Password = new PasswordHash(request.Password),
+                Role = Role.User,
+                CreatedAt = DateTime.UtcNow,
+            };
+        }
+
         public static MemberResponseDTO ToMemberResponseDTO(this Member member)
         {
             return new MemberResponseDTO
