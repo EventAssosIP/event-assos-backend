28008095041d3d49398fafeacb31bcd348be67da
paolo <pao.sarno@gmail.com>
2026-03-30 09:56:31 +0200

    event entity created

---
+++ EventAssos.Domain/Entities/Event.cs
@@ -0,0 +1,34 @@
+﻿using EventAssos.Domain.Enums;
+using EventAssos.Domain.ValueObjects;
+
+namespace EventAssos.Domain.Entities
+{
+    public class Event
+    {
+        public Guid Id { get;  set; }
+
+        public required string Name { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public required DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int MinParticipants { get; set; }
+
+        public int MaxParticipants { get; set; }
+
+        public required EventStatus Status { get; set; }
+
+        public bool WaitingListActive { get; set; }
+
+        public required DateTime RegistrationDeadline { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+     
+}
+
---
+++ EventAssos.Domain/Enums/EventStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace EventAssos.Domain.Enums
+{
+    public enum EventStatus
+    {
+        Delated = 0,
+        Active = 1,
+
+    }
+}
